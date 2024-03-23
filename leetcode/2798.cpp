f54f113b3e2d349846d204da13c71dfb1a0bd9f7
Harry <wisest303@gmail.com>
2024-03-23 19:44:46 +0900

    Create 2798.cpp

---
+++ leetcode/2798.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) {
+        int ret = 0;
+        for(auto h : hours)
+            ret += h >= target;
+        return ret;
+    }
+};
